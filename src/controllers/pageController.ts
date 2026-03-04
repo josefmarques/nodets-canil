6bb74bc0d45053847201c4a75a56b730f5f6c8f3
Jose Marques <josemarques.moc@gmail.com>
2026-03-03 23:15:21 -0300

    Adicionando mais arquivos

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,22 +1,44 @@
 import type { Request, Response } from "express";
 
-export const home = (req: Request, res: Response) => {
-    res.send('home no controller!');
+import { createMenuObject } from '../helpers/createMenuObjects.js';
 
-    // res.render('pages/page)
+export const home = (req: Request, res: Response) => {
+    res.render('pages/page', {
+        menu: createMenuObject('all'),
+        banner: {
+            title: 'Todos os animais',
+            background: 'allanimals.jpg'
+        }        
+    });    
 }
 
 export const dogs = (req: Request, res: Response) => {
+    res.render('pages/page', {
+        menu: createMenuObject('dog'),
+        banner: {
+            title: 'Cachorros',
+            background: 'banner_dog.jpg'
+        }        
+    });   
     
-    // res.render('pages/page)
 }
 
 export const cats = (req: Request, res: Response) => {
-    
-    // res.render('pages/page)
+    res.render('pages/page', {
+        menu: createMenuObject('cat'),
+        banner: {
+            title: 'Gatos',
+            background: 'banner_cat.jpg'
+        }        
+    }); 
 }
 
 export const fishes = (req: Request, res: Response) => {
-    
-    // res.render('pages/page)
+    res.render('pages/page', {
+        menu: createMenuObject('fish'),
+        banner: {
+            title: 'Peixes',
+            background: 'banner_fish.jpg'
+        }        
+    }); 
 }
