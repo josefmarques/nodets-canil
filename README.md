4ecdc1dfa7e711a9666e10fd893e00d448702214
Jose Marques <josemarques.moc@gmail.com>
2026-03-03 21:21:16 -0300

    Base criada

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # nodets-canil
+
+Projeto feito no módulo do curso Node + Typescript
+
+### Pré-requisitos globais 
+`npm install -g nodemon typescript ts-node`
+
+<!-- npm init
+tsc --init
+
+dependencias:
+npm install express mustache-express dotenv
+npm install --save-dev @types/mustache-express @types/node -->
+
+## Para rodar o projeto
+`npm rum start-dev`
+
+
+
+
+
---
+++ src/server.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mustache from 'mustache-express';
+import path from 'path';
+import mainRoutes from './routes/index.js';
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+dotenv.config();
+
+const server = express();
+
+server.set('view engine', 'mustache');
+server.set('views', path.join(__dirname, 'views'));
+server.engine('mustache', mustache());
+
+server.use(express.static(path.join(__dirname, '../public')));
+
+server.use(mainRoutes);
+
+server.use((req, res)=>{
+    res.send('Página não encontrada!');
+});
+
+server.listen(process.env.PORT);
+
+
