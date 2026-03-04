6da43ba2b92cbb32be1f066c1a6f258186302712
Jose Marques <jf-marques@outlook.com>
2026-03-04 20:49:04 -0300

    Final projeto:1.0

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,6 +1,23 @@
 import type { Request, Response } from "express";
 
+import { Pet } from '../models/pet.js';
+import { createMenuObject } from "../helpers/createMenuObjects.js";
+
 export const search = (req: Request, res: Response) => {
-    
-    // res.render('pages/search)
+    let query: string = req.query.q as string;
+
+    if (!query) {
+        res.redirect("/");
+        return;
+    }
+
+
+
+    let list = Pet.getFromName(query);
+
+    res.render('pages/page', {
+        menu: createMenuObject(''),
+        list,
+        query
+    });
 }
